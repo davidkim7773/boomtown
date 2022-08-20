1fd176a13a124f577fcdbe2f834b78b014296fd2
David <scriptura7773@gmail.com>
2022-08-19 23:00:53 -0700

    updated repos component

--- src/components/Repos.jsx
+++ src/components/Repos.jsx
@@ -1,7 +1,30 @@
+import { useState, useEffect } from 'react';
 
 const Repos = (props) => {
+
+  // Deconstruct props ojbect with repoUrl
+  const { repoUrl } = props;
+  // Initalize Repo Component State;
+  const [repoData, setRepoData] = useState({});
+
+  async function fetchRepoData () {
+    const res = await fetch(repoUrl);
+    // Catch errors
+    if (!res.ok) {
+      const msg = `An error has occured with fetchRepoData ${res.status}`
+      throw new Error(msg);
+    }
+    const repoFetchData = await res.json();
+    setRepoData(repoFetchData);
+  }
+  console.log('repo', repoUrl)
+  console.log('repo', repoData);
   
-  
+  useEffect(() => {
+    fetchRepoData();
+  }, []);
+
+
 }
 
 export { Repos }
